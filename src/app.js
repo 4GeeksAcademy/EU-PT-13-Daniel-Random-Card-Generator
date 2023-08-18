26e7998751248a7687fe29b1e9d92f489cfb53c0
ZionSpardas <135459718+ZionSpardas@users.noreply.github.com>
2023-08-18 22:19:47 +0000

    Finished

--- src/app.js
+++ src/app.js
@@ -7,5 +7,62 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  // Simbols
+  let symbol = Math.floor(Math.random() * 4);
+  if (symbol == 0) {
+    document.querySelectorAll(".potato")[0].innerHTML =
+      '<p class="display-2 text-danger">&hearts;</p>';
+    document.querySelectorAll(".potato")[1].innerHTML =
+      '<p class="display-2 text-danger">&hearts;</p>';
+  }
+  if (symbol == 1) {
+    document.querySelectorAll(".potato")[0].innerHTML =
+      '<p class="display-2 text-danger">&diams;</p>';
+    document.querySelectorAll(".potato")[1].innerHTML =
+      '<p class="display-2 text-danger">&diams;</p>';
+  }
+  if (symbol == 2) {
+    document.querySelectorAll(".potato")[0].innerHTML =
+      '<p class="display-2 text-dark">&spades;</p>';
+    document.querySelectorAll(".potato")[1].innerHTML =
+      '<p class="display-2 text-dark">&spades;</p>';
+  }
+  if (symbol == 3) {
+    document.querySelectorAll(".potato")[0].innerHTML =
+      '<p class="display-2 text-dark">&clubs;</p>';
+    document.querySelectorAll(".potato")[1].innerHTML =
+      '<p class="display-2 text-dark">&clubs;</p>';
+  }
+  console.log(symbol);
+  // numbers
+  let number = Math.floor(Math.random() * 10) + 2;
+  if (number == 8) {
+    document.querySelector("#carta").innerHTML = '<p class="display-1">J</p>';
+  } else if (number == 9) {
+    document.querySelector("#carta").innerHTML = '<p class="display-1">Q</p>';
+  } else if (number == 10) {
+    document.querySelector("#carta").innerHTML = '<p class="display-1">K</p>';
+  } else if (number == 11) {
+    // ACES
+    if (symbol == 0) {
+      document.querySelector("#carta").innerHTML =
+        '<p class="display-1 text-danger">&hearts;</p>';
+    }
+    if (symbol == 1) {
+      document.querySelector("#carta").innerHTML =
+        '<p class="display-1 text-danger">&diams;</p>';
+    }
+    if (symbol == 2) {
+      document.querySelector("#carta").innerHTML =
+        '<p class="display-1 text-dark">&spades;</p>';
+    }
+    if (symbol == 3) {
+      document.querySelector("#carta").innerHTML =
+        '<p class="display-1 text-dark">&clubs;</p>';
+    }
+  } else
+    document.querySelector("#carta").innerHTML =
+      '<p class="display-1">' + number + "</p>";
+  console.log(number);
 };
